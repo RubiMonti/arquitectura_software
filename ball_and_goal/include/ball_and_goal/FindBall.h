e3248737afae3b90b793b5b596d833155976ef2f
Veronica274 <v.tornero.27@gmail.com>
2021-04-05 11:49:48 +0200

    Transforms working but have to adjust angle
    Signed-off-by: Veronica274 <v.tornero.27@gmail.com>

--- ball_and_goal/include/ball_and_goal/FindBall.h
+++ ball_and_goal/include/ball_and_goal/FindBall.h
@@ -53,14 +53,11 @@
 
 #include "tf2/transform_datatypes.h"
 #include "tf2_ros/transform_listener.h"
-#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/static_transform_broadcaster.h"
 #include "tf2/LinearMath/Transform.h"
 #include "geometry_msgs/TransformStamped.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "tf2/convert.h"
-
-
-
 
 
 namespace ball_and_goal_bica
@@ -87,7 +84,7 @@ protected:
 
     tf2_ros::Buffer buffer_;
     tf2_ros::TransformListener listener_;
-    tf2_ros::TransformBroadcaster broadcaster;
+    tf2_ros::StaticTransformBroadcaster broadcaster_;
 };
 
 }  // namespace ball_and_goal_bica
