612d11c83daf90e79eb2e2dd258dfcfaa9997be1
Ruben <ruben.montilla.fdez@gmail.com>
2021-04-27 19:15:58 +0200

    Practices in the laboratory with Kobuki
    Signed-off-by: Ruben <ruben.montilla.fdez@gmail.com>

--- hospital_navigation/src/navigation_secuencial.cpp
+++ hospital_navigation/src/navigation_secuencial.cpp
@@ -15,8 +15,12 @@
 #include "ros/ros.h"
 #include "move_base_msgs/MoveBaseAction.h"
 #include "actionlib/client/simple_action_client.h"
+#include "kobuki_msgs/Sound.h"
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+ros::NodeHandle nh_;
+ros::Publisher sound_pub_;
 
 void set_goal(move_base_msgs::MoveBaseGoal& goal, double x, double y)
 {
@@ -44,28 +48,35 @@ void doWork(int num_points, int states, float room1[2], float office1[2], float 
     }
 
     goal.target_pose.header.frame_id = "map";
+    kobuki_msgs::Sound msg;
 
     while (states < num_points)
     {
         if (states == 0)
         {
+            msg.value = 1;
             set_goal(goal, room1[0], room1[1]);
-        }
-        else if (states == 1)
-        {
-            set_goal(goal, reception[0], reception[1]);
-        }
-        else if (states == 2)
-        {
-            set_goal(goal, office1[0], office1[1]);
         }
         else if (states == 3)
         {
+            msg.value = 4;
+            set_goal(goal, reception[0], reception[1]);
+        }
+        else if (states == 1)
+        {
+            msg.value = 2;
+            set_goal(goal, office1[0], office1[1]);
+        }
+        else if (states == 2)
+        {
+            msg.value = 3;
             set_goal(goal, office2[0], office2[1]);
         }
 
         goal.target_pose.header.stamp = ros::Time::now();
-        ROS_INFO("Sending goal");
+        //goal.target_pose.frame_id = "map";
+        ROS_INFO("Sending goal %lf %lf, %lf", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y, goal.target_pose.pose.orientation.w);
+        sound_pub_.publish(msg);
         ac.sendGoal(goal, doneCb);
 
         ac.waitForResult();
@@ -87,14 +98,16 @@ int main(int argc, char** argv)
 {
     int states = 0;
     int num_points = 4;
+    sound_pub_ = nh_.advertise<kobuki_msgs::Sound>("/mobile_base/commands/sound", 1);
+
     ros::init(argc, argv, "navigation");
 
     // ----------- 4 DIFFERENT MAP POINTS ------------ //
 
-    float room1[2] = {-6.13, 8.2};
+    float room1[2] = {-2.88, -0.69};
     float reception[2] = {0.0, 0.0};
-    float office1[2] = {-5.37, 1.13};
-    float office2[2] = {-5.32, -1.37};
+    float office1[2] = {-3.08, 2.68};
+    float office2[2] = {0.16, 2.76};
 
     doWork(num_points, states, room1, office1, office2, reception);
     // ----------------------------------------------- //
