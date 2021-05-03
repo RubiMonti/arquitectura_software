b90edd60f75dd49cf0ab52cb723d3d562bbc5b35
Ruben <ruben.montilla.fdez@gmail.com>
2021-05-03 18:04:00 +0200

    Restored some changes that broke the structure of other proyects
    Signed-off-by: Ruben <ruben.montilla.fdez@gmail.com>

--- behaviour_hospital/src/node_rgbd_distance.cpp
+++ behaviour_hospital/src/node_rgbd_distance.cpp
@@ -29,10 +29,124 @@
 #include <sensor_msgs/PointCloud2.h>
 
 #include <boost/algorithm/string.hpp>
+#include <pcl_ros/transforms.h>
 
+#include "move_base_msgs/MoveBaseAction.h"
+#include "actionlib/client/simple_action_client.h"
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+
+class RGBDFilter
+{
+public:
+  RGBDFilter()
+  {
+    cloud_sub_ = nh_.subscribe("/camera/depth/points", 1, &RGBDFilter::cloudCB, this);
+  }
+
+  void cloudCB(const sensor_msgs::PointCloud2::ConstPtr& cloud_in)
+  {
+    double x,y,z;
+    sensor_msgs::PointCloud2 cloud;
+
+    try
+    {
+      pcl_ros::transformPointCloud("camera_link", *cloud_in, cloud, tfListener_);
+    }
+    catch(tf::TransformException & ex)
+    {
+      ROS_ERROR_STREAM("Transform error of sensor data: " << ex.what() << ", quitting callback");
+      return;
+    }
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcrgb(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::fromROSMsg(cloud, *pcrgb);
+
+    int c_w = cloud_in->width / 2;
+    int c_h = cloud_in->height / 2;
+
+    auto point_3d = pcrgb->at(c_w,c_h);
+    x = point_3d.x;
+    y = point_3d.y;
+    z = point_3d.z;
+
+    //std::cout << "(" << x << "," << y << "," << z << ")" << std::endl;
+  }
+
+private:
+  
+  ros::NodeHandle nh_;
+  ros::Subscriber cloud_sub_;
+  tf::TransformListener tfListener_;
+
+};
+
+void doneCb(const actionlib::SimpleClientGoalState& state,
+            const move_base_msgs::MoveBaseResultConstPtr& result)
+  {
+    ROS_INFO("Finished in state [%s]", state.toString().c_str());
+  }
+
+void set_goal(move_base_msgs::MoveBaseGoal& goal, char* arg)
+  {
+    float x,y;
+    ROS_INFO("ARG = %s\n",arg);
+    
+    x = goal.target_pose.pose.position.x;
+    y = goal.target_pose.pose.position.y;
+
+    if(!(strcasecmp(arg, "room1")))
+    {
+      ROS_INFO("Going to room1\n");
+      x = -6.13;
+      y = 8.2;
+    }
+    else
+    {
+      ROS_INFO("NOTHING RECEIVED\n");
+    }
+
+
+
+    goal.target_pose.pose.orientation.w = 0.0013;
+    
+    
+    
+  }
 
 int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "rgbd_center");
+  RGBDFilter rf;
+
+  move_base_msgs::MoveBaseGoal goal;
+
+  MoveBaseClient ac("move_base", true);
+
+  while (!ac.waitForServer(ros::Duration(5.0)))
+  {
+      ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  goal.target_pose.header.frame_id = "map";
+
+  set_goal(goal, argv[argc-1]);
+  goal.target_pose.header.stamp = ros::Time::now();
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal,doneCb);
+  ac.waitForResult();
   
+
+  if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  {
+      ROS_INFO("Hooray, mission accomplished");
+  }
+  else
+  {
+      ROS_INFO("[Error] mission could not be accomplished");
+  }
+
+  //ros::spin();
   return 0;
 }
